bfd316c3a278a3f93bafe06c8241bcc8ffa31a44
Mati <matiurrehman.meersayed@gmail.com>
2021-09-02 13:11:58 +0530

    gh-page

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from "./App";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
