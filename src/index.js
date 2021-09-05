d53c83bfde072167032097133bff08a93f271f59
Mati <matiurrehman.meersayed@gmail.com>
2021-09-05 19:45:20 +0530

    page

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <StrictMode>
-    <BrowserRoute basename={process.env.PUBLIC_URL}r>
+  <React.StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
-  </StrictMode>,
-  rootElement
+  </React.StrictMode>,
+  document.getElementById("root")
 );
